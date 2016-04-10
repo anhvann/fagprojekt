1c2b1d7e4c95ceca866b5637896a9fc46166eea1
anna0207 <annaoelgaardnielsen@hotmail.com>
2016-04-10 22:34:32 +0200

    No errors in Login.jsp

--- src/fagprojekt/Login.java
+++ src/fagprojekt/Login.java
@@ -32,19 +32,19 @@ public class Login extends HttpServlet {
 		} catch (ClassNotFoundException e) {
 			System.out.println("Driver not found");
 			e.printStackTrace();
-		}		
+		}	
 
 		Properties properties = new Properties();
 		properties.put("user", "DTU12");
 		properties.put("password", "FAGP2016");
-		String url = "jdbc:db2://192.86.32.65:5040/DALLASB:retrieveMessagesFromServerOnGetMessage=true;emulateParameterMetaDataForZCalls=1;";
+		String url = "jdbc:db2://192.86.32.54:5040/DALLASB:retrieveMessagesFromServerOnGetMessage=true;emulateParameterMetaDataForZCalls=1;";
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet rs = null;
 		try {
 			connection = DriverManager.getConnection(url, properties);
 			//statement = connection.createStatement();
-			statement = connection.prepareStatement("select * from USERINFO where email=? and password=?");
+			statement = connection.prepareStatement("select * from DTUGRP05.USERINFO where \"Email\"=? and \"Password\"=?");
 			statement.setString(1, email);
 			statement.setString(2, password);
 			rs = statement.executeQuery();
