062b76ec6741a3fdbd4fd50da238cb25e58e8032
anna0207 <annaoelgaardnielsen@hotmail.com>
2016-04-19 19:07:01 +0200

    Merge

--- src/fagprojekt/Search.java
+++ src/fagprojekt/Search.java
@@ -38,55 +38,6 @@ public class Search extends HttpServlet {
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-		// try {
-		// rs = app.getDatabase();
-		// } catch (SQLException e1) {
-		// System.out.println("ResultSet Empty");
-		// e1.printStackTrace();
-		// }
-		// String search = request.getParameter("Search");
-		// System.out.println("search = " + search);
-		//
-		// ResultSetMetaData rsmd = null;
-		// int numCols;
-		// ArrayList<String> colNames = new ArrayList<String>();
-		// System.out.println("dafuq");
-		// try {
-		// System.out.println("SELECT * FROM DTUGRP05.USERS WHERE \"UserName\"
-		// LIKE '%" + search + "%' OR \"Address\" LIKE '%" + search + "%' OR
-		// \"Email\" LIKE '%" + search + "%' OR \"Phone\" LIKE '%" + search +
-		// "%'");
-		// rs = statement.executeQuery("SELECT * FROM DTUGRP05.USERS");
-		// System.out.println("hej");
-		// rsmd = rs.getMetaData();
-		// numCols = rsmd.getColumnCount();
-		// for (int i = 1; i <= numCols; i++) {
-		// colNames.add(rsmd.getColumnName(i));
-		// }
-		// PrintWriter out = response.getWriter();
-		// out.println("<table><thead><tr>");
-		// for (String name : colNames) {
-		// out.println("<td>"+name+"</td>");
-		// }
-		// out.println("</tr></thead>");
-		// out.println("<tbody>");
-		// while(rs.next()) {
-		// out.println("<tr>");
-		// for (int i = 1; i <= numCols; i++) {
-		// out.println("<td>"+rs.getString(i)+"</td>");
-		// }
-		// out.println("</tr>");
-		// }
-		// out.println("</tbody></table>");
-		// } catch (SQLException e) {
-		// System.out.println("fuck lortet");
-		// e.printStackTrace();
-		// }
-		//
-		//
-=======
-		
 
 		if(results != null && !results.isEmpty()){
 			//Create one long string of IDs for SQL
@@ -105,7 +56,6 @@ public class Search extends HttpServlet {
 			request.setAttribute("message", message);
 			request.getRequestDispatcher("search.jsp").forward(request, response);
 		}
->>>>>>> branch 'master' of https://github.com/anhvann/fagprojekt.git
 	}
 
 	/**
